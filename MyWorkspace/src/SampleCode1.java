56ae619e9c2c4d916070760998dc4926d2949221
hemavarun <vishma.sfdc@gmail.com>
2017-02-12 12:44:23 -0600

    Code push

---
+++ MyWorkspace/src/SampleCode1.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SampleCode1 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int option;
+		Session session = new Session();
+		DateTime dateTime = new DateTime();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter Session ID :");
+		session.ID = Integer.parseInt(reader.readLine());
+		System.out.println("Enter First Name :");
+		session.FirstName = reader.readLine();
+		System.out.println("Enter Last Name :");
+		session.LastName = reader.readLine();
+		System.out.println("Enter description :");
+		session.Description =reader.readLine();
+		session.custDate = dateTime.getDateTime();
+		System.out.println("1. Entry Level");
+		System.out.println("2. Mid Level");
+		System.out.println("3. Professional Level");
+		System.out.println("Enter option :");
+		option = Integer.parseInt(reader.readLine());
+		switch(option){
+		
+		case 1: session.level = "Entry Level";
+				break;
+				
+		case 2: session.level = "Mid Level";
+		        break;
+		
+		case 3: session.level = "Professional Level";
+		        break;
+		        
+		default: System.out.println("Please enter a valid option");  
+		}
+		
+		System.out.println("************************************************************");
+		
+		System.out.println("----------User Details----------");
+		System.out.println("ID : "+ session.ID);
+		System.out.println("First Name : "+ session.FirstName);
+		System.out.println("Last Name : "+ session.LastName);
+		System.out.println("Description : "+session.Description);
+		System.out.println("Date : "+session.custDate);
+		System.out.println("Level : "+session.level);
+		System.out.println("************************************************************");
+		
+		
+		
+	}
+
+}
---
+++ MyWorkspace/src/Session.java
@@ -0,0 +1,9 @@
+
+public class Session {
+	public static int ID;
+	public static String FirstName;
+	public static String LastName;
+	public static String Description;
+	public static String custDate;
+	public static String level;
+}
